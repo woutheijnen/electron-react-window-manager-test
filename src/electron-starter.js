9c64fbcaeb89396299a6ce999cf48b5c668dec55
Wout Heijnen <woutheijnen@hotmail.com>
2018-08-20 00:31:34 +0200

    Fix for electron-starter.js errors

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,7 +1,11 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const electron = require("electron");
 // Module to control application life.
 const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const url = require("url");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -12,10 +16,10 @@ function createWindow() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  mainWindow.loadUrl("http://localhost:3000");
+  mainWindow.loadURL("http://localhost:3000");
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on("closed", function() {
