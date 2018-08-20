64ec1a7b889c6d0a8cb53a272c4606da23e65cf7
Wout Heijnen <wheijnen.externe@altima-assurances.fr>
2018-08-20 12:23:45 +0200

    Installation electron-renderer-react-scripts-target pour fix compatibilité electron et react sans avoir besoin de faire un eject

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-const electron = window.require("electron");
+import electron from 'electron'
 
 class App extends Component {
-  render() {
+  render () {
     // Get screen displays
-    const electronScreen = electron.screen;
+    const electronScreen = electron.screen
     // Get all connected monitors
-    const displays = electronScreen.getAllDisplays();
-    console.log(displays);
+    const displays = electronScreen.getAllDisplays()
+    console.log(displays)
     // List the IDs
     const displayIDs = displays.map((element, key) => (
       <li key={key}>
@@ -25,23 +25,23 @@ class App extends Component {
         TouchSupport: {element.touchSupport}
         <br />
       </li>
-    ));
+    ))
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React/Electron</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React/Electron</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           Hello Electron! There are currently {displays.length} screens
           connected to your PC.
         </p>
         <p>Screen IDs:</p>
         <ol>{displayIDs}</ol>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
